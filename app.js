5ceb6d51dc367b2a9171381a97e51f5f01c9a453
RichardPickman <drezzerock@gmail.com>
2022-05-29 19:26:57 +0200

    feat: add classes for BEM

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="todo-list__image";
     deleteButton.appendChild(deleteButtonImg);
 
 
