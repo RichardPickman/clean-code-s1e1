262e628d075a5862dc836f67614e5377fba94d1d
RichardPickman <drezzerock@gmail.com>
2022-05-30 16:09:54 +0200

    refactor: change buttons to BEM - rule 2.1 html&css advanced

--- app.js
+++ app.js
@@ -43,9 +43,9 @@ var createNewTaskElement=function(taskString){
     editInput.type="text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="list__edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="list__delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="list__image";
     deleteButton.appendChild(deleteButtonImg);
@@ -87,7 +87,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".list__edit");
     var containsClass=listItem.classList.contains("list__item--edit");
     //If class of the parent is .editmode
     if(containsClass){
@@ -160,8 +160,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector("button.list__edit");
+    var deleteButton=taskListItem.querySelector("button.list__delete");
 
 
     //Bind editTask to edit button.
